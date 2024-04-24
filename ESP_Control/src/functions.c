c116bf2a999a5009972fe020773b0218fa3f13cb
Michał Kowalski <248959@student.pwr.edu.pl>
2024-04-24 20:51:16 +0200

    Align voltage readout

--- ESP_Control/src/functions.c
+++ ESP_Control/src/functions.c
@@ -33,7 +33,8 @@ void initPeripherals() {
 void measure() {
 
     // Measure battery voltage with ADC:
-    moduleData.dataToObc.power_voltage = adc1_get_raw((adc1_channel_t)ADC1_CHANNEL_6) * 3.3 / 4095 * 3;
+    moduleData.dataToObc.power_voltage = adc1_get_raw((adc1_channel_t)ADC1_CHANNEL_6) * 3.3 / 4095 * 2.2;
+    ESP_LOGI(TAG, "Batt: %f\n", moduleData.dataToObc.power_voltage);
 
     if (moduleData.obcState >= COUNTDOWN && moduleData.obcState <= SECOND_STAGE_RECOVERY) {
 
