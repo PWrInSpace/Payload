b2c9e57fde0c37977cf7614583c0b44973ad7584
Michał Kowalski <248959@student.pwr.edu.pl>
2024-04-28 23:39:09 +0200

    SLOT A and B in flash works

---
+++ F411_Green_Miszur/Core/Src/Functions.c
@@ -0,0 +1,43 @@
+#include "Functions.h"
+
+uint32_t dotOneMsTime;
+volatile bool tickTime;
+uint8_t rocketState;
+
+void doMeasurements(Frame* frame) {
+
+	frame->time = dotOneMsTime;
+	frame->state = rocketState;
+
+	for (uint16_t i = 0; i < ADC_SIZE; i ++) {
+
+		// Czekanie 10us:
+		while (!tickTime);
+		tickTime = 0;
+
+		HAL_ADC_PollForConversion(&hadc1, 1);
+		frame->adc[i][0] = HAL_ADC_GetValue(&hadc1);
+		HAL_ADC_PollForConversion(&hadc1, 1);
+		frame->adc[i][1] = HAL_ADC_GetValue(&hadc1);
+		HAL_ADC_PollForConversion(&hadc1, 1);
+		frame->adc[i][2] = HAL_ADC_GetValue(&hadc1);
+	}
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+
+	if (htim->Instance == TIM1) {
+		tickTime = 1;
+		dotOneMsTime += 1;
+	}
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    // Check if UART1:
+    if (huart->Instance == USART1)
+    {
+        // Next message IT request:
+        HAL_UART_Receive_IT(&huart1, &rocketState, 1);
+    }
+}
